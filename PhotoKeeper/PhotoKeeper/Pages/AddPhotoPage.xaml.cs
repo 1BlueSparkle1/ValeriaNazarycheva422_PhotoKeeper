ca2fd96cbbad4fe414d590ee2e72768ef9565451
LERA <LERA@WIN-FL0E7E95RSD>
2025-03-01 11:50:15 +0300

    project finished

--- PhotoKeeper/PhotoKeeper/Pages/AddPhotoPage.xaml.cs
+++ PhotoKeeper/PhotoKeeper/Pages/AddPhotoPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using PhotoKeeper.Components;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,63 @@ namespace PhotoKeeper.Pages
     /// </summary>
     public partial class AddPhotoPage : Page
     {
+        private Photo photo;
         public AddPhotoPage()
         {
             InitializeComponent();
+            photo = new Photo();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            photo.UserId = App.thisUser.Id;
+            photo.Description = DescriptionTb.Text;
+            App.db.Photo.Add(photo);
+            App.db.SaveChanges();
+            if (PetCb.SelectedIndex == 0)
+            {
+                Photo_Pet photo_Pet = new Photo_Pet();
+                photo_Pet.PhotoId = App.db.Photo.ToList().Last().Id;
+                photo_Pet.PetId = 2;
+                App.db.Photo_Pet.Add(photo_Pet);
+                App.db.SaveChanges();
+            }
+            else if (PetCb.SelectedIndex == 1)
+            {
+                Photo_Pet photo_Pet = new Photo_Pet();
+                photo_Pet.PhotoId = App.db.Photo.ToList().Last().Id;
+                photo_Pet.PetId = 1;
+                App.db.Photo_Pet.Add(photo_Pet);
+                App.db.SaveChanges();
+            }
+            else
+            {
+                Photo_Pet photo_Pet = new Photo_Pet();
+                photo_Pet.PhotoId = App.db.Photo.ToList().Last().Id;
+                photo_Pet.PetId = 1;
+                App.db.Photo_Pet.Add(photo_Pet);
+                App.db.SaveChanges();
+                Photo_Pet photo_Pet2 = new Photo_Pet();
+                photo_Pet2.PhotoId = App.db.Photo.ToList().Last().Id;
+                photo_Pet2.PetId = 2;
+                App.db.Photo_Pet.Add(photo_Pet2);
+                App.db.SaveChanges();
+            }
+            MessageBox.Show("Сохранено!");
+            Navigations.valuePage(new ListImagePage());
+        }
+
+        private void EditImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog()
+            {
+                Filter = "*.png|*.png|*.jpg|*.jpg|*.jpeg|*.jpeg"
+            };
+            if (openFile.ShowDialog().GetValueOrDefault())
+            {
+                photo.ImageBite = File.ReadAllBytes(openFile.FileName);
+                MainImage.Source = new BitmapImage(new Uri(openFile.FileName));
+            }
         }
     }
 }
--- PhotoKeeper/PhotoKeeper/Pages/TitlePage.xaml.cs
+++ PhotoKeeper/PhotoKeeper/Pages/TitlePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PhotoKeeper.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,7 @@ namespace PhotoKeeper.Pages
 
         private void AddBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            Navigations.valuePage(new AddPhotoPage());
         }
     }
 }
